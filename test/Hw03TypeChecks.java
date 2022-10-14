50277e7f04dfbb1061eae14612f2d12b72298129
beenbiishop <ben@benbishopsb.com>
2022-10-14 08:56:38 -0400

    Fix style

--- test/Hw03TypeChecks.java
+++ test/Hw03TypeChecks.java
@@ -5,21 +5,29 @@ import cs3500.marblesolitaire.model.hw02.EnglishSolitaireModel;
  */
 public class Hw03TypeChecks {
 
+  /**
+   * Given main method to test the compilation of code.
+   *
+   * @param args the arguments taken in by the main method
+   */
   public static void main(String[] args) {
     Readable rd = null;
     Appendable ap = null;
-    helper(new EnglishSolitaireModel(),
-            rd, ap);
-    helper(new EnglishSolitaireModel
-                    (3, 3),
-            rd, ap);
+    helper(new EnglishSolitaireModel(), rd, ap);
+    helper(new EnglishSolitaireModel(3, 3), rd, ap);
   }
 
-  private static void helper
-          (cs3500.marblesolitaire.model.hw02.MarbleSolitaireModel model,
-           Readable rd,Appendable ap) {
+  /**
+   * Helper method to test the compilation of code.
+   *
+   * @param model the model to be played
+   * @param rd    the input to be read
+   * @param ap    the output to be written
+   */
+  private static void helper(cs3500.marblesolitaire.model.hw02.MarbleSolitaireModel model,
+      Readable rd, Appendable ap) {
     new cs3500.marblesolitaire.controller.MarbleSolitaireControllerImpl(model,
-            new cs3500.marblesolitaire.view.MarbleSolitaireTextView(model,ap),rd);
+        new cs3500.marblesolitaire.view.MarbleSolitaireTextView(model, ap), rd);
   }
 
 }
